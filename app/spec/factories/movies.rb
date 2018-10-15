62411146fe6d3685ab7610baa5c3100f0ba00d20
kashiwabara <m_kashiwabara@bbmedia.co.jp>
2018-10-15 16:59:11 +0900

    Movieモデルのテストを修正 & 不要なメソッド等削除

--- app/spec/factories/movies.rb
+++ app/spec/factories/movies.rb
@@ -2,6 +2,16 @@ FactoryBot.define do
   factory :movie do
     sequence(:title) { |n| "TEST_MOVIE_TITLE#{n}"}
     sequence(:url) { |n| "TEST_MOVIE_URL#{n}.com"}
+    publish {true}
     feed
+    movie_type
   end
+  factory :movie_unpublish, class: Movie do
+    sequence(:title) { |n| "TEST_MOVIE_TITLE#{n}"}
+    sequence(:url) { |n| "TEST_MOVIE_URL#{n}.com"}
+    publish {false}
+    feed
+    movie_type
+  end
+
 end
--- app/spec/models/movie_spec.rb
+++ app/spec/models/movie_spec.rb
@@ -1,24 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe Movie, type: :model do
-
   describe "データベース接続ができている場合" do
     context "データの保存が" do
      let(:movie) {
-        @feed = build(:feed) do |f|
-          f.title = "TestSite"
-        end
-        @feed.save()
-        @movie_type = create(:movie_type)
-        Movie.new(title:"TestMovie",url:"test-movie.com",feed_id: @feed.id, movie_type_id: @movie_type.id)
+        feed = create(:feed, title:"TestFeedTitle")
+        build(:movie, feed_id: feed.id)
       }
       it "正常にできる" do
         expect(movie).to be_valid
-        expect(movie.feed.title).to eq "TestSite"
-        expect(movie.publish).to eq true
+        expect(movie.feed.title).to eq "TestFeedTitle"
+      end
+    end
+    context "データが全て未公開の場合" do
+      before do
+        create(:movie_unpublish)
+        create(:movie_unpublish)
+      end
+      it "公開データの数は0になる" do
+        expect(Movie.all.published.count).to eq 0
       end
     end
   end
-
-
 end
