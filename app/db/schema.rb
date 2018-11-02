f26b543506b704b302895c68482ec2cf9b38639b
kashiwabara <m_kashiwabara@bbmedia.co.jp>
2018-11-02 15:33:15 +0900

    bundle update

--- app/db/schema.rb
+++ app/db/schema.rb
@@ -12,9 +12,6 @@
 
 ActiveRecord::Schema.define(version: 2018_10_24_083859) do
 
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
   create_table "favicons", force: :cascade do |t|
     t.binary "image"
     t.string "image_type"
